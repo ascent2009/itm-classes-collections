4a0b009aa2ed9bf07b7fe0fa5ffcfec9fee8572f
ascent2009 <ascent2009@hotmail.com>
2023-05-18 17:32:08 +0300

    makePairs solved

--- objects/makePairs.js
+++ objects/makePairs.js
@@ -6,7 +6,8 @@
  */
 
 export const makePairs = (object) => {
-    throw new Error(`Напишите здесь свое решение ${object}`);
+  // throw new Error(`Напишите здесь свое решение ${object}`);
+  return Object.entries(object);
 };
 
 const data = { a: 1, b: 2 };
