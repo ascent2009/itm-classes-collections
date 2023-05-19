1924e55257e2b90cc654c38f73ca8b9c76f575e3
ascent2009 <ascent2009@hotmail.com>
2023-05-19 18:09:24 +0300

    gettersAndSetters solved

--- classes/gettersAndSetters.js
+++ classes/gettersAndSetters.js
@@ -4,17 +4,47 @@
  */
 
 class Worker {
+  #name;
+  #surname;
+  #rate;
+  #days;
+  constructor(name, surname, rate, days) {
+    this.#name = name;
+    this.#surname = surname;
+    this.#rate = rate;
+    this.#days = days;
+  }
 
+  getName() {
+    return this.#name;
+  }
+  getSurname() {
+    return this.#surname;
+  }
+  getRate() {
+    return this.#rate;
+  }
+  getDays() {
+    return this.#days;
+  }
+  getSalary() {
+    return this.#rate * this.#days;
+  }
+  setRate(value) {
+    this.#rate = value;
+  }
+  setDays(value) {
+    this.#days = value;
+  }
 }
 
-const worker = new Worker('Иван', 'Иванов', 10, 31);
+const worker = new Worker("Иван", "Иванов", 10, 31);
 
 console.log(worker.getName()); //выведет 'Иван'
 console.log(worker.getSurname()); //выведет 'Иванов'
 console.log(worker.getRate()); //выведет 10
 console.log(worker.getDays()); //выведет 31
 console.log(worker.getSalary()); //выведет 310 - то есть 10*31
-
 
 /**
  * Модифицируйте класс Worker из предыдущей задачи следующим образом: для свойства rate и для свойства days сделайте еще и методы-сеттеры.
